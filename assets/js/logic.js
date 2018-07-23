24fbc77fd4fffb77b79395e9dac6bb7c84d6e5a5
ntaifour <37583771+ntaifour@users.noreply.github.com>
2018-07-23 08:14:00 -0400

    portfolio structure updates

---
+++ assets/js/logic.js
@@ -0,0 +1,7 @@
+// Start splashbody logic
+
+    // window.onload = function() { document.body.classList.remove('is-preload'); }
+    // window.ontouchmove = function() { return false; }
+    // window.onorientationchange = function() { document.body.scrollTop = 0; }
+
+// End splashbody logic
